5599d0ab19d5fb1331896bc65a33d4b26ff9b7e2
Nick Bonnet <njbuwm@gmail.com>
2019-01-26 18:36:23 -0500

    Removed unnecessary usings

--- Tagger/MetadataParser.cs
+++ Tagger/MetadataParser.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.IO.Abstractions;
-using System.Linq;
+﻿using System.IO.Abstractions;
 
 namespace Tagger
 {
